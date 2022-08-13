6aa6c872dde9c8f9d3c40f74026c76e9c99f5ff9
hafizurrahmanrazib <hafizur201611044@gmail.com>
2022-08-14 00:01:16 +0600

    object done

---
+++ object.js
@@ -0,0 +1,13 @@
+const student = {
+    name: 'Sakib Khan',
+    id: 121,
+    address: 'movie cinema',
+    isSingle: true,
+    friends: ['Apu', 'Raaz', 'Salman', 'aamir'],
+    car: {
+        brand: 'tesla',
+        price: 50000000,
+        made: 2021
+    }
+}
+console.log(student);
