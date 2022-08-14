5660097093cc62b6ebd729f5b25d4727f247d8d9
hafizurrahmanrazib <hafizur201611044@gmail.com>
2022-08-15 05:46:42 +0600

    js core concepts bonus module done

--- matched.js
+++ matched.js
@@ -4,9 +4,38 @@ for (let i = 0; i < numbers.length; i++) {
     console.log(number);
 } */
 
-// New Technique in JavaScript For Getting Array Element easily
+// New Technique in JavaScript For Getting Array Element easily by loop through
 // For Of Loop
-const numbers = [45, 65, 23, 98, 19];
+/* const numbers = [45, 65, 23, 98, 19];
 for (const number of numbers) {
     console.log(number);
+} */
+
+const products = [
+    { id: 1, name: 'xiami phone one night', price: 19000 },
+    { id: 2, name: 'iphone', price: 19000 },
+    { id: 3, name: 'mac book air', price: 119000 },
+    { id: 4, name: 'lenovo yoga laptop 2025', price: 19000 },
+    { id: 5, name: 'Dell inspiron Laptop', price: 19000 },
+    { id: 6, name: 'Samsung Phone note 7', price: 19000 },
+    { id: 7, name: 'Nokia old age Phone gone', price: 19000 },
+    { id: 8, name: 'One Plus Phone', price: 19000 },
+    { id: 9, name: 'M1 chip not cheap LapTOP', price: 19000 }
+]
+
+// for (const product of products) {
+//     console.log(product);
+// }
+
+function matchedProducts(products, search) {
+    const matched = [];
+    for (const product of products) {
+        if (product.name.toLowerCase().includes(search.toLowerCase())) {
+            matched.push(product);
+        }
+    }
+    return matched;
 }
+
+const result = matchedProducts(products, 'laptop');
+console.log(result);
